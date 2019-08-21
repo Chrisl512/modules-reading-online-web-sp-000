5a4af834f3a008d1659f540251cab753c67b6f43
Christopher Luna <chris@illusionlayervfx.com>
2019-08-21 03:22:11 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,11 +4,4 @@ require_relative './dance_module.rb'
 class Dancer
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
 end
